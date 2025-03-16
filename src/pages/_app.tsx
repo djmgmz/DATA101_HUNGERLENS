be0a66a345869d901d4dbd60315cf1cb86bb7cab
Dominic Gomez <dominic_joel_gomez@dlsu.edu.ph>
2025-03-17 00:12:43 +0800

    chore: initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ChakraProvider, Box, Flex } from "@chakra-ui/react";
+import Sidebar from "../components/sidebar";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Flex>
+        <Sidebar />
+
+        <Box flex="1" p={4}>
+          <Component {...pageProps} />
+        </Box>
+      </Flex>
+    </ChakraProvider>
+  );
 }
---
+++ src/pages/hungertrendsovertime.tsx
@@ -0,0 +1,20 @@
+import {
+    Box,
+    Heading,
+    Text,
+    VStack,
+    HStack,
+    Flex,
+  } from "@chakra-ui/react";
+  
+  export default function WorldMap() {
+    return (
+      <Box p={4} bg="gray.100" minHeight="100vh">
+        <Heading textAlign="center" color="goldenrod" mb={6}>
+          HUNGER TRENDS OVER TIME
+        </Heading>
+  
+      </Box>
+    );
+  }
+  
